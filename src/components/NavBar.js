9f3557804ebd44e02663767547c8badb659fb928
Liam-WB <>
2024-02-04 22:33:30 +0000

    -Add NavBar logo, NavBar bootstrap code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
+import logo from '../assets/element-logo.png'
 
 const NavBar = () => {
     return (
         <Navbar expand="md" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand>
+                    <img src={logo} alt='logo' height='45'/>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav className="ml-auto text-left">
+                        <Nav.Link>
+                            <i className='fas fa-home'></i>Home
+                        </Nav.Link>
+                        <Nav.Link>
+                            <i className='fas fa-sign-in-alt'></i>Sign In
+                        </Nav.Link>
+                        <Nav.Link>
+                            <i className='fas fa-user-plus'></i>Sign Up
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
