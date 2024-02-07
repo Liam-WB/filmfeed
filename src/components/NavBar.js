25a69a4e5e2ba4057293cc30487ba02737f9f3f5
Liam-WB <>
2024-02-07 21:07:22 +0000

    -Change workspace/website name/colour theme -Switch site logo, favicon assets

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
-import logo from '../assets/element-logo.png'
+import logo from '../assets/filmfeed-logo.png'
 import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
-            <Container>
-                <Navbar.Brand>
-                    <img src={logo} alt='logo' height='45'/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto text-left">
-                        <Nav.Link>
-                            <i className='fas fa-home'></i>Home
-                        </Nav.Link>
-                        <Nav.Link>
-                            <i className='fas fa-sign-in-alt'></i>Sign In
-                        </Nav.Link>
-                        <Nav.Link>
-                            <i className='fas fa-user-plus'></i>Sign Up
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+            <Navbar.Brand>
+                <img src={logo} alt='logo' height='50'/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto text-left">
+                    <Nav.Link>
+                        <i className='fas fa-home'></i>Home
+                    </Nav.Link>
+                    <Nav.Link>
+                        <i className='fas fa-sign-in-alt'></i>Sign In
+                    </Nav.Link>
+                    <Nav.Link>
+                        <i className='fas fa-user-plus'></i>Sign Up
+                    </Nav.Link>
+                    <Nav.Link>
+                        <i class="fa-solid fa-envelope"></i>Contact Us
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 };
