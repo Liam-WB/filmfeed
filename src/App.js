23f80da01052a57494e14b5e5c91197284762156
Liam-WB <>
2024-02-07 23:46:11 +0000

    -Add links to NavBar anchors, invalid url page, react router/switch imports, import syntax

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
             <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
             <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
             <Route exact path="/contactus" render={() => <h1>Contact Us</h1>} />
+            <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,28 +2,44 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import logo from '../assets/filmfeed-logo.png';
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
-            <Navbar.Brand>
-                <img src={logo} alt='logo' height='50'/>
-            </Navbar.Brand>
+            <NavLink to="/">
+                <Navbar.Brand>
+                    <img src={logo} alt='logo' height='50'/>
+                </Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <Nav.Link>
+                    <NavLink 
+                    exact
+                    className={styles.NavLink} 
+                    activeClassName={styles.Active} 
+                    to="/">
                         <i className='fas fa-home'></i>Home
-                    </Nav.Link>
-                    <Nav.Link>
+                    </NavLink>
+                    <NavLink 
+                    className={styles.NavLink} 
+                    activeClassName={styles.Active} 
+                    to="/signin">
                         <i className='fas fa-sign-in-alt'></i>Sign In
-                    </Nav.Link>
-                    <Nav.Link>
+                    </NavLink>
+                    <NavLink 
+                    className={styles.NavLink} 
+                    activeClassName={styles.Active}
+                    to="/signup">
                         <i className='fas fa-user-plus'></i>Sign Up
-                    </Nav.Link>
-                    <Nav.Link>
+                    </NavLink>
+                    <NavLink 
+                    className={styles.NavLink} 
+                    activeClassName={styles.Active} 
+                    to="/contactus">
                         <i class="fa-solid fa-envelope"></i>Contact Us
-                    </Nav.Link>
+                    </NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
