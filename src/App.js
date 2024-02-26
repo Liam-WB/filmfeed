27be0f4e08a54cf14d398f55cb6724349374e658
Liam Blake <liamwartnerblake@gmail.com>
2024-02-26 15:16:34 +0000

    -Add onchange event handler for sign up form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -8,6 +8,20 @@ import appStyles from "../../App.module.css";
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: '',
+    password1: '',
+    password2: ''
+  })
+const {username, password1, password2} = signUpData;
+
+const handlechange = (event) => {
+    setSignUpData({
+        ...signUpData,
+        [event.target.name]: event.target.value
+    })
+}
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
@@ -19,20 +33,20 @@ const SignUpForm = () => {
                 <Form.Label className="d=none">username</Form.Label>
                 <Form.Control 
                     className={styles.Input}
-                    type="text" placeholder="Username" name="username" />
+                    type="text" placeholder="Username" name="username" value={username} onChange={handleChange} />
             </Form.Group>
 
             <Form.Group controlId="password1">
                 <Form.Label className="d=none">Password</Form.Label>
                 <Form.Control 
                     className={styles.Input}
-                    type="password" placeholder="Password" name="password1" />
+                    type="password" placeholder="Password" name="password1" value={password1} onChange={handleChange} />
             </Form.Group>
             <Form.Group controlId="password2">
                 <Form.Label className="d=none">Confirm password</Form.Label>
                 <Form.Control 
                     className={styles.Input}
-                    type="password" placeholder="Confirm password" name="password2" />
+                    type="password" placeholder="Confirm password" name="password2" value={password2} onChange={handleChange} />
             </Form.Group>
             <Button className={'${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}'} type="submit">
                 Sign Up
