732702afef9c0b12d8e8c6f97abecf38c4da4dbd
Liam Blake <liamwartnerblake@gmail.com>
2024-03-23 20:59:26 +0000

    -Style image borders for signInUpForms -Add temp image src urls

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -115,7 +115,7 @@ function SignInForm() {
       >
         <Image
           className={`${appStyles.FillerImage}`}
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
+          src="https://media.istockphoto.com/id/1494642262/photo/people-in-the-cinema-auditorium-with-empty-white-screen.jpg?s=612x612&w=0&k=20&c=wiVYHafqEAlvufaCpOTZhn9wuklrgKHdDHWqpmMGhjw="
         />
       </Col>
     </Row>
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -131,7 +131,7 @@ const SignUpForm = () => {
       >
         <Image
           className={`${appStyles.FillerImage}`}
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"}
+          src="https://wallpapercave.com/wp/wp1945897.jpg"
         />
       </Col>
     </Row>
