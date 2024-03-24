53ace7b16f409f7e5234af54f0f7c56eb3ec0954
Liam Blake <liamwartnerblake@gmail.com>
2024-03-24 00:19:36 +0000

    -Style movies page results

--- src/pages/movies/MoviesPage.js
+++ src/pages/movies/MoviesPage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import styles from "../../styles/MoviesPage.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import appstyles from "../../App.module.css"
 import { Button } from "react-bootstrap";
 import {apiKey} from "../../apikey";
 import { axiosCustom } from "../../api/axiosDefaults";
@@ -53,29 +54,31 @@ function MoviesPage() {
       </div>
 
       { Object.keys (data).length > 0 &&
-        <div className="mt-10 w-full flex items-center justify-center">
-        <div>
-          <img src={data.Poster} alt="#" />
-        </div>
-        <div className="ml-5 bg-slate-200">
-          <h1>Title: {data.Title}</h1>
-          <div className="pt-2" />
-          <p>Director: {data.Director}</p>
-          <div className="pt-2" />
-          <p>Genre: {data.Genre}</p>
-          <div className="pt-2" />
-          <p>Year: {data.Year}</p>
-          <div className="pt-2" />
-          <p>Country: {data.Country}</p>
-          <div className="pt-2" />
-          <p>Actors: {data.Actors}</p>
-          <div className="pt-2" />
-          <p>Language: {data.Language}</p>
-          <div className="pt-2" />
-          <p>Rating: {data.imdbRating}</p>
-          <div className="pt-2" />
-          <p>Plot: {data.Plot}</p>
-          <div className="pt-2" />
+      <div className={appstyles.NewContent}>
+          <div className="mt-10 w-full flex items-center justify-center">
+          <div>
+            <img className={styles.MoviePoster} src={data.Poster} alt="#" />
+          </div>
+          <div className="ml-5 bg-slate-200">
+            <h1>Title: {data.Title}</h1>
+            <div className="pt-2" />
+            <p>Director: {data.Director}</p>
+            <div className="pt-2" />
+            <p>Genre: {data.Genre}</p>
+            <div className="pt-2" />
+            <p>Year: {data.Year}</p>
+            <div className="pt-2" />
+            <p>Country: {data.Country}</p>
+            <div className="pt-2" />
+            <p>Actors: {data.Actors}</p>
+            <div className="pt-2" />
+            <p>Language: {data.Language}</p>
+            <div className="pt-2" />
+            <p>Rating: {data.imdbRating}</p>
+            <div className="pt-2" />
+            <p>Plot: {data.Plot}</p>
+            <div className="pt-2" />
+          </div>
         </div>
       </div>
       }
